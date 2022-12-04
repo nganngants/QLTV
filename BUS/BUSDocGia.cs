83cb1bd3fe664c21a53b08d7cb9ec8430778ff8f
Chirox03 <21520474@gm.uit.edu.vn>
2022-12-04 22:19:07 +0700

    DOCGIA, LOAIDOCGIA BUS

---
+++ BUS/BUSDocGia.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    internal class BUSDocGia
+    {
+        private static BUSDocGia instance;
+        public static BUSDocGia Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BUSDocGia();
+                return instance;
+            }
+            set => instance = value;     
+        }
+        public List<DOCGIA> GetAllDocGia()
+        {
+            return DALDocGia.Instance.GetAllDocGia();
+        }
+        public DOCGIA GetDocGia(string id)
+        {
+            return DALDocGia.Instance.GetDocGiaById(id);
+        }
+        public bool DelDocGia(string id)
+        {
+            DOCGIA dg = DALDocGia.Instance.GetDocGiaById(id);
+            foreach( PhieuMuon in dg.PHIEUMUONTRA)
+            {
+                //
+                return false;
+            }
+            DALDocGia.Instance.DelDocGia(id);
+            return true;
+        }
+    }
+}
---
+++ BUS/BUSLoaiDocGia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    internal class BUSLoaiDocGia
+    {
+        private static BUSLoaiDocGia instance;
+        public static BUSLoaiDocGia Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BUSLoaiDocGia();
+                return instance;
+            }
+            set => instance = value;
+        }
+        public List<LOAIDOCGIA> GetAllLoaiDocGia()
+        {
+            return DALLoaiDocGia.Instance.GetAllLoaiDocgia();
+        }
+        public bool DelLoaiDocGia(string id)
+        {
+            return (DALLoaiDocGia.Instance.DelLoaiDocGia(id)) ;
+        }
+        public bool AddLoaiDocGia(string name)
+        {
+            return (DALLoaiDocGia.Instance.AddLoaiDocGia(name));
+        }
+    }
+}
