317b75123f90411d5f6a518092c33ba29279d68c
Elise <21520069@gm.uit.edu.vn>
2022-12-08 23:02:53 +0700

    add DALThamSo

--- DAL/DALThamSo.cs
+++ DAL/DALThamSo.cs
@@ -20,11 +20,161 @@ namespace DAL
             }
             set => instance = value;
         }
-        public DALThamSo() { }
+
+        public DALThamSo()
+        {
+            if (!QLTVDb.Instance.THAMSOes.Any()) 
+            {
+                QLTVDb.Instance.THAMSOes.Add(new THAMSO
+                {
+                    TuoiToiThieu = 18,
+                    TuoiToiDa = 55,
+                    ThoiHanThe = 6,
+                    KhoangCachXuatBan = 8,
+                    SoSachMuonToiDa = 5,
+                    SoNgayMuonToiDa = 4,
+                    DonGiaPhat = 1000,
+                    AD_QDKTTienThu = 1
+                });
+                QLTVDb.Instance.SaveChanges();
+            }
+        }
         public THAMSO GetAllThamSo()
         {
-            return QLTVDb.Instance.ThamSo.ToList();
+
+            return QLTVDb.Instance.THAMSOes.First();
         }
-        
+
+        public bool UpdTuoiToiThieu (int tuoi)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.TuoiToiThieu = tuoi;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+            
+        }
+        public bool UpdTuoiToiDa(int tuoi)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.TuoiToiDa = tuoi;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public bool UpdThoiHanThe(int thoiHan)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.ThoiHanThe = thoiHan;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public bool UpdKhoangCachXB(int kc)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.KhoangCachXuatBan = kc;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+
+        }
+
+        public bool UpdSoSachMuonToiDa(int soSach)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.SoSachMuonToiDa = soSach;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+
+        }
+
+        public bool UpdSoNgayMuonToiDa(int soNgay)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.SoNgayMuonToiDa = soNgay;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+
+        }
+
+        public bool UpdDonGiaPhat(int donGia)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.DonGiaPhat = donGia;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public bool UpdQDKTTienPhat(int apDung)
+        {
+            try
+            {
+                THAMSO ts = GetAllThamSo();
+                ts.AD_QDKTTienThu = apDung;
+                QLTVDb.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+
+        }
     }
 }
