c9c03adcdbc3b4d9051d9d1914459eb59adade75
BichLoan <21521083@gm.uit.edu.vn>
2022-12-08 14:39:30 +0700

    Change Password

--- GUI/fChangePassword.Designer.cs
+++ GUI/fChangePassword.Designer.cs
@@ -36,17 +36,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.siticoneControlBox2 = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
             this.siticoneControlBox1 = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
-            this.siticoneTextBox3 = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
-            this.siticoneHtmlLabel1 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
-            this.siticoneButton1 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.label2 = new System.Windows.Forms.Label();
             this.siticoneContainerControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // siticoneContainerControl1
             // 
-            this.siticoneContainerControl1.Controls.Add(this.siticoneButton1);
-            this.siticoneContainerControl1.Controls.Add(this.siticoneHtmlLabel1);
-            this.siticoneContainerControl1.Controls.Add(this.siticoneTextBox3);
+            this.siticoneContainerControl1.Controls.Add(this.label2);
             this.siticoneContainerControl1.Controls.Add(this.butLogin);
             this.siticoneContainerControl1.Controls.Add(this.siticoneTextBox5);
             this.siticoneContainerControl1.Controls.Add(this.siticoneTextBox2);
@@ -54,7 +50,7 @@
             this.siticoneContainerControl1.Controls.Add(this.label1);
             this.siticoneContainerControl1.Location = new System.Drawing.Point(100, 100);
             this.siticoneContainerControl1.Name = "siticoneContainerControl1";
-            this.siticoneContainerControl1.Size = new System.Drawing.Size(600, 600);
+            this.siticoneContainerControl1.Size = new System.Drawing.Size(600, 500);
             this.siticoneContainerControl1.TabIndex = 1;
             this.siticoneContainerControl1.Text = "siticoneContainerControl1";
             // 
@@ -68,11 +64,11 @@
             this.butLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.butLogin.ForeColor = System.Drawing.Color.Black;
             this.butLogin.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.butLogin.Location = new System.Drawing.Point(220, 496);
+            this.butLogin.Location = new System.Drawing.Point(227, 410);
             this.butLogin.Name = "butLogin";
             this.butLogin.Size = new System.Drawing.Size(150, 50);
             this.butLogin.TabIndex = 11;
-            this.butLogin.Text = "Reset";
+            this.butLogin.Text = "Lưu";
             // 
             // siticoneTextBox5
             // 
@@ -85,7 +81,7 @@
             this.siticoneTextBox5.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
             this.siticoneTextBox5.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.siticoneTextBox5.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.siticoneTextBox5.Location = new System.Drawing.Point(105, 404);
+            this.siticoneTextBox5.Location = new System.Drawing.Point(107, 336);
             this.siticoneTextBox5.Name = "siticoneTextBox5";
             this.siticoneTextBox5.PasswordChar = '\0';
             this.siticoneTextBox5.PlaceholderText = "Confirm ";
@@ -104,7 +100,7 @@
             this.siticoneTextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
             this.siticoneTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.siticoneTextBox2.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.siticoneTextBox2.Location = new System.Drawing.Point(105, 324);
+            this.siticoneTextBox2.Location = new System.Drawing.Point(107, 256);
             this.siticoneTextBox2.Name = "siticoneTextBox2";
             this.siticoneTextBox2.PasswordChar = '\0';
             this.siticoneTextBox2.PlaceholderText = "New Password";
@@ -123,10 +119,10 @@
             this.siticoneTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
             this.siticoneTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.siticoneTextBox1.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.siticoneTextBox1.Location = new System.Drawing.Point(105, 150);
+            this.siticoneTextBox1.Location = new System.Drawing.Point(107, 143);
             this.siticoneTextBox1.Name = "siticoneTextBox1";
             this.siticoneTextBox1.PasswordChar = '\0';
-            this.siticoneTextBox1.PlaceholderText = "Your Email";
+            this.siticoneTextBox1.PlaceholderText = "Your Password";
             this.siticoneTextBox1.SelectedText = "";
             this.siticoneTextBox1.Size = new System.Drawing.Size(400, 50);
             this.siticoneTextBox1.TabIndex = 6;
@@ -134,12 +130,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F);
-            this.label1.Location = new System.Drawing.Point(180, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36.288F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(248, 95);
+            this.label1.Size = new System.Drawing.Size(544, 72);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Login";
+            this.label1.Text = "Change Password";
             // 
             // siticoneControlBox2
             // 
@@ -164,56 +160,21 @@
             this.siticoneControlBox1.Size = new System.Drawing.Size(60, 60);
             this.siticoneControlBox1.TabIndex = 6;
             // 
-            // siticoneTextBox3
+            // label2
             // 
-            this.siticoneTextBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.siticoneTextBox3.DefaultText = "";
-            this.siticoneTextBox3.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.siticoneTextBox3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.siticoneTextBox3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.siticoneTextBox3.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.siticoneTextBox3.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
-            this.siticoneTextBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.siticoneTextBox3.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.siticoneTextBox3.Location = new System.Drawing.Point(105, 247);
-            this.siticoneTextBox3.Name = "siticoneTextBox3";
-            this.siticoneTextBox3.PasswordChar = '\0';
-            this.siticoneTextBox3.PlaceholderText = "Code";
-            this.siticoneTextBox3.SelectedText = "";
-            this.siticoneTextBox3.Size = new System.Drawing.Size(229, 50);
-            this.siticoneTextBox3.TabIndex = 12;
-            // 
-            // siticoneHtmlLabel1
-            // 
-            this.siticoneHtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.siticoneHtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.216F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.siticoneHtmlLabel1.Location = new System.Drawing.Point(105, 219);
-            this.siticoneHtmlLabel1.Name = "siticoneHtmlLabel1";
-            this.siticoneHtmlLabel1.Size = new System.Drawing.Size(158, 22);
-            this.siticoneHtmlLabel1.TabIndex = 13;
-            this.siticoneHtmlLabel1.Text = "Please enter the code";
-            // 
-            // siticoneButton1
-            // 
-            this.siticoneButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.siticoneButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.siticoneButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.siticoneButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.siticoneButton1.FillColor = System.Drawing.Color.LightGray;
-            this.siticoneButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.siticoneButton1.ForeColor = System.Drawing.Color.Black;
-            this.siticoneButton1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.siticoneButton1.Location = new System.Drawing.Point(355, 247);
-            this.siticoneButton1.Name = "siticoneButton1";
-            this.siticoneButton1.Size = new System.Drawing.Size(150, 50);
-            this.siticoneButton1.TabIndex = 14;
-            this.siticoneButton1.Text = "Check";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.216F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Please enter your new password";
             // 
             // fChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.ClientSize = new System.Drawing.Size(800, 700);
             this.Controls.Add(this.siticoneControlBox2);
             this.Controls.Add(this.siticoneControlBox1);
             this.Controls.Add(this.siticoneContainerControl1);
@@ -236,8 +197,6 @@
         private System.Windows.Forms.Label label1;
         private Siticone.Desktop.UI.WinForms.SiticoneControlBox siticoneControlBox2;
         private Siticone.Desktop.UI.WinForms.SiticoneControlBox siticoneControlBox1;
-        private Siticone.Desktop.UI.WinForms.SiticoneTextBox siticoneTextBox3;
-        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton1;
-        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel siticoneHtmlLabel1;
+        private System.Windows.Forms.Label label2;
     }
 }
