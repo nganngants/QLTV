b99c5f890ac796b290c35e0299c6da322e7289af
Chirox03 <21520474@gm.uit.edu.vn>
2022-12-12 16:34:56 +0700

    BC theo the loai

--- BUS/BUSBCTheoTheLoai.cs
+++ BUS/BUSBCTheoTheLoai.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,38 @@ namespace BUS
 {
     internal class BUSBCTheoTheLoai
     {
+        private static BUSBCTheoTheLoai instance;
+        public static BUSBCTheoTheLoai Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BUSBCTheoTheLoai();
+                return instance;
+            }
+            set => instance = value;
+        }
+        public List<BCLUOTMUONTHEOTHELOAI> GetAllBC()
+        {
+            return DALBCLuotMuonTheoTheLoai.Instance.GetAllBaoCao();
+        }
+        public string AddBC(DateTime NgayLap)
+        {
+            int thang = NgayLap.Month;
+            int nam = NgayLap.Year;
+            if (NgayLap > DateTime.Now) return "Ngày báo cáo không hợp lệ";
+            if (DALBCLuotMuonTheoTheLoai.Instance.AddBaoCao(thang, nam))
+            {
+                List<BCLUOTMUONTHEOTHELOAI> bc = DALBCLuotMuonTheoTheLoai.Instance.FindBaoCaoByDate(thang, nam);
+                List<THELOAI> ltl = DALTheLoai.Instance.GetAllTheLoai();
+                foreach (THELOAI tl in ltl)
+                {
+                    if (BUSCT_BCLuotMuonTheoThang.Instance.AddCT_BCLuotMuon(bc[0].id, tl.id, thang, nam) == "")
+                        continue;
+                }    
+            }    
+            return "KHông thể lập báo cáo";
+        }
 
+        
     }
 }
---
+++ BUS/BUSCT_BCLuotMuonTheoThang.cs
@@ -0,0 +1,51 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    internal class BUSCT_BCLuotMuonTheoThang
+    {
+        private static BUSCT_BCLuotMuonTheoThang instance;
+        public static BUSCT_BCLuotMuonTheoThang Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BUSCT_BCLuotMuonTheoThang();
+                return instance;
+            }
+            set => instance = value;
+        }
+        public List<CT_BCLUOTMUONTHEOTHELOAI> GetAllCT_BCLuotMuon()
+        {
+            return DALCT_BCLuotMuonTheoTheLoai.Instance.GetAllCTBC();
+        }
+        public string AddCT_BCLuotMuon(int id, int idTL, int month, int year)
+        {
+            int soLuot = 0;
+            THELOAI tl = DALTheLoai.Instance.GetTheLoaiById(idTL);
+            List<TUASACH> lts = DALTuaSach.Instance.FindTuaSach(null, tl, null);
+            foreach(TUASACH ts in lts)
+             foreach(SACH sach in ts.SACHes)
+                {
+                    foreach(CUONSACH cs in sach.CUONSACHes.ToList())
+                    {
+                        foreach(PHIEUMUONTRA pmt in cs.PHIEUMUONTRAs)
+                        {
+                            DateTime nm = (DateTime)pmt.NgayMuon;
+                            if (nm.Month == month && nm.Year == year)
+                                soLuot++;
+                        }
+                    }
+                }
+            if (DALCT_BCLuotMuonTheoTheLoai.Instance.AddCTBC(id, tl.id, soLuot))
+                return "";
+            return "Lỗi";
+
+        }
+    }
+}
